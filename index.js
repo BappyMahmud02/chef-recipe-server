626067d363c896d745ab344e332179c05a180551
Bappy <bappymahmud321@gmail.com>
2023-05-01 23:32:02 +0600

    create server site data

--- index.js
+++ index.js
@@ -4,9 +4,14 @@ const app = express()
 const port = 5000
 app.use(cors())
 
+const data = require('./data/data.json');
 
 app.get('/', (req, res) => {
   res.send('Hello World!dadadadad')
+})
+
+app.get('/data', (req, res) => {
+    res.send(data)
 })
 
 app.listen(port, () => {
