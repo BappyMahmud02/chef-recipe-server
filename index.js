6608bae42973387811095c00f962c553428b7f46
Bappy <bappymahmud321@gmail.com>
2023-05-03 15:19:42 +0600

    added

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 const express = require('express')
 const cors = require('cors')
 const app = express()
-const port = 5000
+const port = process.env.PORT || 5000 ;
 app.use(cors())
 
-const data = require('./data/data.json');
+const chef = require('./data/data.json');
 
 app.get('/', (req, res) => {
   res.send('Hello World!dadadadad')
 })
 
 app.get('/chefs', (req, res) => {
-  console.log(data);
-    res.send(data);
+  console.log(chef);
+    res.send(chef);
 })
 
 app.listen(port, () => {
